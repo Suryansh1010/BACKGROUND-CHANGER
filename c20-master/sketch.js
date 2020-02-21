f973dadcce07c6251a403c6d5cc410a18365e710
Suryansh1010 <60614718+Suryansh1010@users.noreply.github.com>
2020-02-21 12:23:29 +0530

    Add files via upload

---
+++ c20-master/sketch.js
@@ -0,0 +1,43 @@
+
+var r = 0;
+var g = 50;
+var b=255;
+
+
+// INITIALIZE ALL YOUR VARIABLES
+// Ideally you would create 3 variables. red blue green or r, g, b
+
+
+function setup(){
+  createCanvas(800,400);
+}
+
+
+function draw(){
+background("yellow");
+  
+
+if(mouseX>200){
+  background("red");
+}
+
+if(mouseX<600){
+  background("green");
+}
+
+if(mouseIsPressed){
+  background("blue");
+}
+
+
+  // change the value of Red based on the mouse movement about the X axis
+  // change the value of Green based on the mouse movement about the X axis
+  // change the value of Blue based on the mouse movement about the X axis
+
+  // Use the map() function to do so. 
+
+  // Pass the values to the background() function you have learnt previously.
+
+  // Create an ellipse that will move around with you mouse about the X axis.
+  // Remember to fill the canvas with white paint before creating the ellipse.
+}
